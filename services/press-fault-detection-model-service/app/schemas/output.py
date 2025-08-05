f4da41b9fea3d464cba3b7bfe706c501ac8e5969
SoYeon <soye0200@gmail.com>
2025-08-05 17:29:19 +0900

    ✨ Feat: change input preprocessing, add output
    입력 시퀀스를 슬라이딩 윈도우 방식으로 변경, 예측 오차 값 출력 추가

--- services/press-fault-detection-model-service/app/schemas/output.py
+++ services/press-fault-detection-model-service/app/schemas/output.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from typing import Dict, Optional
 
 class PredictionResponse(BaseModel):
     prediction: str
     reconstruction_error: float
     is_fault: bool
+    attribute_errors: Optional[Dict[str, float]] = None
