bb9c166068a08b5e0fd218a988d943314afcd91f
SoYeon <soye0200@gmail.com>
2025-08-11 17:33:00 +0900

    ✨ feat: implement logger
    - 로깅 시스템 구현
    - 고장 예측 시 로그를 json 파일로 저장할 수 있도록 구현

--- services/press-fault-data-simulator-service/app/main.py
+++ services/press-fault-data-simulator-service/app/main.py
@@ -3,14 +3,15 @@ from contextlib import asynccontextmanager
 import os
 
 from app.config.settings import settings
+from app.utils.logger import system_log
 
-app = FastAPI(
-    title="유압 펌프 고장 예측 시뮬레이터",
-    description=""
-)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # 1. 로그 디렉토리 생성
-    if not os.path.exists(settings.LOG_DIR):
-        os.makedirs(settings.LOG_DIR, exist_ok=True)
+
+    system_log.info(f"애플리케이션 시작 준비 완료.")
+    yield
+    system_log.info(f"애플리케이션이 종료됩니다.")
+
+
+app = FastAPI(title="유압 펌프 고장 예측 시뮬레이터", description="", lifespan=lifespan)
